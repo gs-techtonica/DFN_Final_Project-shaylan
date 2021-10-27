e8392de6d8f7f563710817ecc7d4a61db4c9a271
Shaylan Hawthorne <shaylan.hawthorne@gmail.com>
2021-10-27 15:08:32 -0700

    adjustments in npm package to get react bootsrap components rendering

--- app/src/Distance/index.js
+++ app/src/Distance/index.js
@@ -1,77 +1,59 @@
 import * as React from "react";
 
+import "./homepage.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
 import useApi from "../auth/useApi";
 
-import "./homepage.css";
+import DonationSites from "./donationsites";
 import MapContainer from "./map";
 
 // import styles from "./styles.module.scss";
 
 const Distance = () => {
   const { loading, apiClient } = useApi();
-  const [distance, setDistance] = React.useState([]);
   const [origin, setOrigin] = React.useState("");
-  console.log(distance);
+  const [distance, setDistance] = React.useState([]);
 
   const loadDistance = async () =>
     setDistance(await apiClient.getDistance(origin));
-
-  //   React.useEffect(() => {
-  //     !loading && loadDistance();
-  //   }, [origin]);
 
   const onSubmit = (e) => {
     e.preventDefault();
     loadDistance();
   };
 
-  // const canAdd = origin !== "";
-
-  // const onSubmit = (e) => {
-  //     e.preventDefault();
-  //     if (canAdd) {
-  //         addOrigin(origin);
-  //         setOrigin("");
-  //     }
-  // };
-
   return loading ? null : (
-    <div className="homepagecontainer">
-      <form {...{ onSubmit }}>
-        <label>
-          Enter Current Address:{" "}
-          <input
-            onChange={(e) => setOrigin(e.currentTarget.value)}
-            value={origin}
-          />
-        </label>
-        <button>Search</button>
-        <div className="mapcontainer">
-          <MapContainer address={addressforMap}/>
-          {/* on backend before we return result, sort the array by distance. Front
-        end will show order. Return just first two indexes. */}
-          {/* google map react element pass addresses */}
+    <Container>
+      <div>
+        <div className="search">
+          <form {...{ onSubmit }}>
+            <label>
+              Enter Current Address:{" "}
+              <input
+                onChange={(e) => setOrigin(e.currentTarget.value)}
+                value={origin}
+              />
+            </label>
+            <button>Search</button>
+          </form>
         </div>
-        <div className="distanceresponse">
-          {distance.slice(0, 3).map((element) => {
-            let distanceMiles = (
-              element.distance.replace("km", "") * 0.621371
-            ).toFixed(2);
-            let addressforMap = element.address;
-            console.log(addressforMap);
-            return (
-              <div className="distancecard">
-                <h4>{element.name}</h4>
-                <p>
-                  {element.address},{element.duration}, {distanceMiles} miles,{" "}
-                  {element.phone_number},{" "}
-                </p>
-              </div>
-            );
-          })}
-        </div>
-      </form>
-    </div>
+        <Row>
+          <Col>
+            <DonationSites distance={distance} />
+          </Col>
+
+          <Col>
+            <MapContainer distance={distance} />
+          </Col>
+        </Row>
+      </div>
+    </Container>
   );
 };
 
--- app/src/Distance/map.js
+++ app/src/Distance/map.js
@@ -6,24 +6,22 @@ const style = {
   height: "50%",
 };
 
-export class MapContainer extends React.Component {
-  render() {
-    return (
-      <Map google={this.props.google} zoom={14} style={style}>
-        <Marker
-          name={"Dolores park"}
-          position={{ lat: 37.759703, lng: -122.428093 }}
-        />
-        {/* pass distance object as a prop from index.js and use lat: this.props.distance.lat and this.props.distance.lng */}
-        {/* <InfoWindow onClose={this.onInfoWindowClose}>
+const MapContainer = (props) => {
+  return (
+    <Map google={props.google} zoom={14} style={style}>
+      <Marker
+        name={"Dolores park"}
+        position={{ lat: 37.759703, lng: -122.428093 }}
+      />
+      {/* pass distance object as a prop from index.js and use lat: this.props.distance.lat and this.props.distance.lng */}
+      {/* <InfoWindow onClose={this.onInfoWindowClose}>
           <div>
             {/* <h1>{this.state.selectedPlace.name}</h1> */}
-        {/* </div>
+      {/* </div>
         </InfoWindow> */}
-      </Map>
-    );
-  }
-}
+    </Map>
+  );
+};
 
 export default GoogleApiWrapper({
   apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
